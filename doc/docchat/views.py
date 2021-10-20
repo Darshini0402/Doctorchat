99e16d7a70cb6ffbb6adefb161ea3aadb573a26d
Darshini0402 <82100004+Darshini0402@users.noreply.github.com>
2021-10-20 21:21:13 +0530

    login logout basic

--- doc/docchat/views.py
+++ doc/docchat/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.http.response import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+
+def login_view(request):
+    if request.method == "POST":
+        # Accessing username and password from form data
+        username = request.POST["username"]
+        password = request.POST["password"]
+        # Check if username and password are correct, returning User object if so
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            #return render(request,'quiz.html')
+            return HttpResponseRedirect(reverse("template"))
+            # Otherwise, return login page again with new context
+        else:
+            return render(request, "login.html", {
+                "message": "Invalid Credentials"
+            })
+    return render(request, "login.html")
+
+def logout_view(request):
+    logout(request)
+    return render(request, "login.html", {
+                "message": "Logged Out"
+            })
+
+def template(request):
+    return render(request,'template.html')
+
+def user(request):
+    return render(request,'User.html')
